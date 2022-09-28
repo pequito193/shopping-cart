550ebf47951b5914c4ca6d39c27179c6117f60ae
pequito193 <vitorspequito@gmail.com>
2022-09-28 15:51:40 +0100

    Finish header element

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import './../styles/Header.css'
 
 function Header() {
 
+    return (
+        <React.Fragment>
+            <header>
+                <p className="header-element" >Homepage</p>
+                <p className="header-element" >Catalog</p>
+                <img src={require("./../images/shopping-cart.png")} className="header-icon" alt="" />
+            </header>
+        </React.Fragment>
+    )
 }
 
 export default Header;
