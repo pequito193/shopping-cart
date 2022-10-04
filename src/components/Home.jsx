0a7745bf794ada9212f5d0405d0ead310cf792a6
pequito193 <vitorspequito@gmail.com>
2022-10-04 16:32:23 +0100

    Finish home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import './../styles/Home.css'
+import { Link } from "react-router-dom";
 
 function Home() {
 
     return (
         <React.Fragment>
-            <div className="home-main"></div> 
+            <div className="home-main">
+                <div className="image-div">
+                    <span className="troll">Nothing suspicious going on here.</span>
+                </div>
+                <div className="right-side-wrapper">
+                    <div className="quote-wrapper">
+                        <em className="quote">"Geographers never get lost. They just do accidental field work"</em>
+                        <p className="author">- Nicholas Chrisman</p>
+                    </div>
+                    <Link to='/shopping-cart/catalog'><button className="shop-now">Shop Now</button></Link>
+                </div>
+            </div> 
         </React.Fragment>
     )
 }
