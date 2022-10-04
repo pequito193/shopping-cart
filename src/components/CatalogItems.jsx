c30b54b3633caaa9c4a564a8d110745ce3a61822
pequito193 <vitorspequito@gmail.com>
2022-10-04 15:20:41 +0100

    Reorder catalog

--- src/components/CatalogItems.jsx
+++ src/components/CatalogItems.jsx
@@ -1,15 +1,15 @@
 let catalogItems = [
-    ['andes.webp', '$16.00', 'Andes and Amazon Basin'],
     ['australia.webp', '$14.50', 'Eastern Australia'],
-    ['china.webp', '$18.50', 'Eastern China'],
-    ['europe.webp', '$16.00', 'Europe'],
-    ['india.webp', '$18.50', 'Indian Subcontinent'],
     ['iran.jpg', '$14.50', 'Iranian Plateau'],
-    ['korea-japan.webp', '$16.00', 'Korean and Japan'],
-    ['mexico.jpg', '$18.50', 'Mexico'],
     ['middle-east.jpg', '$14.50', 'Middle East'],
-    ['patagonia.jpg', '$16.00', 'Patagonia'],
     ['scandinavia.jpg', '$14.50', 'Scandinavia'],
+    ['andes.webp', '$16.00', 'Andes and Amazon Basin'],
+    ['europe.webp', '$16.00', 'Europe'],
+    ['korea-japan.webp', '$16.00', 'Korean and Japan'],
+    ['patagonia.jpg', '$16.00', 'Patagonia'],
+    ['china.webp', '$18.50', 'Eastern China'],
+    ['india.webp', '$18.50', 'Indian Subcontinent'],
+    ['mexico.jpg', '$18.50', 'Mexico'],
     ['southeast-asia.jpg', '$18.50', 'South-East Asia']
 ]
 
