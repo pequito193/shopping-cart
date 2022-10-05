20777162d7f73e34de13bfb41b98b37fa17d2fa9
pequito193 <vitorspequito@gmail.com>
2022-10-05 15:10:29 +0100

    Fix parent child component relationships

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Home from './components/Home';
-import Catalog from './components/Catalog';
-import Cart from './components/Cart';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Main from './components/Main';
 
 function App() {
 
   return (
     <React.Fragment>
         <Header />
-        <Routes>
-          <Route exact path="/shopping-cart/" element={<Home />} />
-          <Route exact path="/shopping-cart/catalog" element={<Catalog />} />
-          <Route exact path="/shopping-cart/cart" element={<Cart />} />
-        </Routes>
+        <Main />
         <Footer />
     </React.Fragment>
   );
